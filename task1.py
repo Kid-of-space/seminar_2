69c793c8aa171bad60f1e1a9c1ec4f98210f7bc2
Gadzhi <zgadjiz@icloud.com>
2023-07-24 08:27:28 +0300

    add task1

--- task1.py
+++ task1.py
@@ -1,9 +1,16 @@
-n = int(input("Введите число: "))
-summ = 0
+# S = int(input('Введите сумму чисел: '))
+# P = int(input('Введите произведение чисел: '))
+#
+# X = (S-int((S**2-4*P)**0.5))//2
+# Y = S - X
+# if X<=1000 and Y<=1000:
+#     print('Петя обманул')
+# print(f'числа задуманные Петей{X, Y}')
 
-while n > 0:
-    x = n % 10
-    summ = x + summ
-    n = n // 10
-    
-print(summ)
+
+
+# N = int(input('Введите целое число больше 1: '))
+# k=1
+# while k<=N:
+#     print(k,end=' ')
+#     k=k*2
